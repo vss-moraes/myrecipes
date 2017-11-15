6651972e1ddfdd48c128704e22e2889e4424e0c7
Vinicius Moraes <vsantana0212@gmail.com>
2017-11-15 19:37:31 +0000

    Add TDD and index listing

---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class RecipesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
+    @recipe = Recipe.create!(name: "vegetable sautee", description: "great vegetable sautee", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken sautee", description: "great chicken dish")
+    @recipe2.save
+  end
+
+  test "should get recipes index" do
+    get recipes_path
+    assert_response :success
+  end
+  
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
+  end
+
+end
